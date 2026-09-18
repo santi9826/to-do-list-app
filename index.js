6f7acc1825d35ecda4b11bc3595a12f916549269
Grace Santillan <gracesantillan992@gmail.com>
2026-09-18 19:27:52 +0300

    index.js modified

---
+++ index.js
@@ -0,0 +1,22 @@
+const taskbox = document.getElementById("taskbox");
+const submit = document.getElementById("submit");
+const all = document.getElementById("all");
+const active = document.getElementById("active");
+const completed = document.getElementById("completed");
+const task1 = document.getElementById("task1");
+const task2 = document.getElementById("task2");
+const task3 = document.getElementById("task3");
+const task4 = document.getElementById("task4");
+const task5 = document.getElementById("task5");
+const task6 = document.getElementById("task6");
+const task7 = document.getElementById("task7");
+const task8 = document.getElementById("task8");
+const task9 = document.getElementById("task9");
+const task10 = document.getElementById("task10");
+
+
+function task(){
+   task1.textContent = `${taskbox.value}`;
+   
+
+}
